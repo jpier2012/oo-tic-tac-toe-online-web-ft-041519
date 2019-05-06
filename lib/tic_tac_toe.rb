b50f4b8745d28adf44d656d2c80d2962e783c3fd
James Pier <jpier2012@gmail.com>
2019-05-06 14:58:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,7 @@ class TicTacToe
     if !input.is_a?(Integer)
       begin
         raise InputError
-        rescue input_to_index
+      rescue puts "Please enter a valid number", input_to_index
       end
     else
       input
