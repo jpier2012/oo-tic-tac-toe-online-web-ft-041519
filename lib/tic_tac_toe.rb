4abb1820580d75625dcd31ae5a37256007700a5d
James Pier <jpier2012@gmail.com>
2019-05-06 15:07:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ class TicTacToe
 
   def input_to_index
     input = gets.chomp.to_i - 1
-    until input.is_a?(Integer)
+    until input.is_a?(Integer) && input <= 9 && input >= 1
       input_to_index
     end
   end
