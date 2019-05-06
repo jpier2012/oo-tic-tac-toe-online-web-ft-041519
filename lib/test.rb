7572cd781a42ae7d5e0feb925c69e99e8c1a65d4
James Pier <jpier2012@gmail.com>
2019-05-06 17:07:27 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -3,15 +3,4 @@ require_relative 'tic_tac_toe'
 tic = TicTacToe.new
 
 #tic.display_board
-
-tic.turn
-tic.turn
-tic.turn
-tic.turn
-tic.turn
-tic.turn
-tic.turn
-tic.turn
-tic.turn
-
 puts tic.won?
